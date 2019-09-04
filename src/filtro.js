0529906d4e5529cd31429c3142e41d621801a77c
Uriel <Leiru06@SECPARTDIR.local>
2019-09-04 16:57:00 -0500

    Mejora de manejo de imagenes

--- src/filtro.js
+++ src/filtro.js
@@ -1,20 +1,31 @@
 let filtro;
-
+let imagen = new Image();
 function recibeColor(color){
-	filtro = color.value;
+        filtro = color.value;
 }
-function recibeImagen(){
-	let input = document.getElementById("files");
-	let archivo = input.files[0];
-	let imagen = new Image();
-	imagen.src = archivo.name;
+function recibeImagen(evento){
+	let archivo = evento.files[0];
+	if(archivo.type.match('image.*')){
+		let lector = new FileReader();
+		lector.onload = (function() {
+			return function(e) {
+			  let canvas = document.getElementById("imagen");
+			  let ctx = canvas.getContext("2d");
+			  imagen.src = e.target.result;
+			  ctx.canvas.width = imagen.naturalWidth;
+			  ctx.canvas.height = imagen.naturalHeight;
+			  ctx.drawImage(imagen,0,0);
+			};
+		})(archivo);
+		lector.readAsDataURL(archivo);
+	}
+}
+
+function aplicaFiltro(){
+        
+}
+function borraImagen(){
 	let canvas = document.getElementById("imagen");
 	let ctx = canvas.getContext("2d");
-	imagen.onload = function(){
-		ctx.canvas.width = imagen.naturalWidth;
-		ctx.canvas.height = imagen.naturalHeight;
-		ctx.drawImage(imagen,0,0);
-}
-function aplicaFiltro(){
-	}
+	ctx.clearRect(0,0,canvas.width,canvas.height);
 }
