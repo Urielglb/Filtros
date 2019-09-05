92c4d531cb69adfef5c2baaa3335c3bf45708347
Mauricio Riva <mauricioriva@ciencias.unam.mx>
2019-09-04 19:36:40 -0500

    Filtro inverso listo

--- src/filtro.js
+++ src/filtro.js
@@ -1,8 +1,10 @@
 let filtro;
 let imagen = new Image();
+
 function recibeColor(color){
         filtro = color.value;
 }
+
 function recibeImagen(evento){
 	let archivo = evento.files[0];
 	if(archivo.type.match('image.*')){
@@ -22,8 +24,23 @@ function recibeImagen(evento){
 }
 
 function aplicaFiltro(){
-        
+	
+	var c = document.getElementById("imagen");
+	var ctx = c.getContext("2d");
+	var imgData = ctx.getImageData(0, 0, c.width, c.height);
+	
+	// invert colors
+	for (var i = 0; i < imgData.data.length; i += 4) {
+	  imgData.data[i] = 255-imgData.data[i];
+	  imgData.data[i+1] = 255-imgData.data[i+1];
+	  imgData.data[i+2] = 255-imgData.data[i+2];
+	  imgData.data[i+3] = 255;
+	}
+	
+	ctx.putImageData(imgData, 0, 0); 
+
 }
+
 function borraImagen(){
 	let canvas = document.getElementById("imagen");
 	let ctx = canvas.getContext("2d");
